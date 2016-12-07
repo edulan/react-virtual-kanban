25d538cce569293a4f6e41478f08c11608b2e161
Eduardo Lanchares <eduardo@elanchares.com>
2016-12-07 15:06:01 +0100

    Remove PropTypes for production builds

--- src/demo/index.js
+++ src/demo/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Perf from 'react-addons-perf';
 
+// TODO: Bundle react virtualized styles inside styles.css
+import 'react-virtualized/styles.css';
 import '../styles.css';
 
 import { generateLists } from './utils/generate_lists';
@@ -11,6 +13,6 @@ import App from './App';
 window.Perf = Perf;
 
 ReactDOM.render(
-  <App lists={generateLists(20, 10)} />,
+  <App lists={generateLists(20, 5)} />,
   document.getElementById('root')
 );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import Kanban from './Kanban';
 
-import 'react-virtualized/styles.css';
-
 export { Kanban as VirtualKanban };
