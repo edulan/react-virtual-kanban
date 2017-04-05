c2e60f029d172adaadf4b32a37e4eb54b5104659
Jorge Morante <jorge@morante.eu>
2017-04-05 16:08:32 +0200

    Expose all drag(begin|end)(row|list) callbacks

--- src/SortableList/dragSpec.js
+++ src/SortableList/dragSpec.js
@@ -1,9 +1,19 @@
 export function beginDrag(props) {
-  return {
+  const data = {
     list: props.list,
     listId: props.listId,
     listStyle: props.listStyle
   };
+
+  props.dragBeginList(data);
+
+  return data;
+}
+
+export function endDrag(props, monitor) {
+  const { listId } = props;
+
+  props.dragEndList({listId});
 }
 
 export function isDragging({ listId }, monitor) {
